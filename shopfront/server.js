ada0886b96bef0b43ccf215a2caee50217d777b7
CMooney-01 <chris.mooney@blackflagbarbell.com.au>
2021-09-29 18:44:36 +1000

    Built out basic page structure for login, signup pages. Added placeholder product category pages.

--- shopfront/server.js
+++ shopfront/server.js
@@ -9,18 +9,37 @@ app.get('/', function(req, res) {
   res.render('pages/index');
 })
 
+// Skateboard deck category page
 app.get('/skateboards', function(req, res) {
   res.render('pages/skateboards');
 })
-
+// Longboard deck category page
+app.get('/longboards', function(req, res) {
+  res.render('pages/longboards');
+})
+// Wheels and trucks category page
+app.get('/wheelstrucks', function(req, res) {
+  res.render('pages/wheelstrucks');
+})
+// Swag category page
+app.get('/swag', function(req, res) {
+  res.render('pages/swag');
+})
+// Contact us page
 app.get('/contact', function(req, res) {
   res.render('pages/contact');
 })
-
 app.post('/contact-sent', function(req, res) {
   res.render('pages/contact-sent');
 })
-
+// Login page
+app.get('/login', function(req, res) {
+  res.render('pages/login');
+})
+// Signp page
+app.get('/signup', function(req, res) {
+  res.render('pages/signup');
+})
 
 
 app.listen(3000);
